34d8536c265fbfd74b4f2d51fbe4ea4ca58dffae
Savaliya Nevil <savaliyanevil9@gmail.com>
2024-03-08 09:02:01 +0530

    Update DatabaseConnection.php

--- DatabaseConnection.php
+++ DatabaseConnection.php
@@ -3,12 +3,13 @@
 error_reporting(0);
 session_start();
 $servername = "0.tcp.in.ngrok.io";
+$port = 16247;
 $username = "root";
 $password = "";
 $dbname = "homeserviceprovider1";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname,$port);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
